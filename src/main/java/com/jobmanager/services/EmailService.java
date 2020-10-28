45d3299bed09b2f7689c62afcde179d676c26f3f
sousajo <josesousa@rentalcars.com>
2020-10-28 00:19:07 +0000

    WIP impl service. Closes

---
+++ src/main/java/com/jobmanager/services/EmailService.java
@@ -0,0 +1,14 @@
+package com.jobmanager.services;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+/**
+ * Created by sousaJ on 28/10/2020
+ * in package - com.jobmanager.services
+ **/
+@Service
+@Transactional
+public class EmailService {
+}

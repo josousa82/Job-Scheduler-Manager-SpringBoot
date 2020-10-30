bbed48833ea0ffd213cf754a2dc730e288fe3311
sousajo <josesousa@rentalcars.com>
2020-10-30 02:29:37 +0000

    WIP

---
+++ src/main/java/com/jobmanager/dynamicqwartzjobs/QuartzProperties.java
@@ -0,0 +1,11 @@
+package com.jobmanager.dynamicqwartzjobs;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by sousaJ on 30/10/2020
+ * in package - com.jobmanager.dynamicqwartzjobs
+ **/
+@ConfigurationProperties("com.jobmanager.dynamicqwartzjobs")
+public class QuartzProperties {
+}
